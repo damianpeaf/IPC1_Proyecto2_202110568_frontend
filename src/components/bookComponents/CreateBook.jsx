e94dd32856f2231e2bc8c5c2b79ff8c13eebe812
Damian <damianpeaf@gmail.com>
2022-04-05 16:16:07 -0600

    Discarting the crazy idea

--- src/components/bookComponents/CreateBook.jsx
+++ src/components/bookComponents/CreateBook.jsx
@@ -26,14 +26,6 @@ function CreateBook() {
                 <button type="button" onClick={handleLessRegister} className="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 shadow-lg shadow-red-500/50 dark:shadow-lg dark:shadow-red-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">-1</button>
             </div>
 
-            <div className="flex justify-center my-5">
-                <button className="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-green-400 to-blue-600 group-hover:from-green-400 group-hover:to-blue-600 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800">
-                    <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
-                        Crear Todos
-                    </span>
-                </button>
-            </div>
-
             <div className="flex">
                 {[...Array(numberOfRegisters)].map((e, i) => <BookForm bookData={{}} type="create" key={i} />)}
 
